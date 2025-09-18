781dcd281c28786ba2985099fd04ebf9c70657b8
Elendayer <nhoehnk@gmail.com>
2025-09-18 14:31:50 +0200

    [Fix] Fixed Error in a Trigger call

--- Assets/Scripts/Gameplay/GameplayManager/TurnManager.cs
+++ Assets/Scripts/Gameplay/GameplayManager/TurnManager.cs
@@ -43,14 +43,15 @@ public class TurnManager : MonoBehaviour
     private void OnTurnStart()
     {
 
-        GameEvents.TriggerRefEvent(new TriggerRef() { Reference = gameplayRef.onTurnStart,TargetId = TurnOrder[CurrentTurnIndex].GetInstanceID()});
+        GameEvents.TriggerRefEvent(new TriggerRef(new() { gameplayRef.onTurnStart }, TurnOrder[CurrentTurnIndex].GetInstanceID()));
+
         Debug.Log($" {gameplayRef.onTurnStart}, TargetId = {TurnOrder[CurrentTurnIndex].GetInstanceID()}");
 
         DeckManager.Instance.StartTurn(TurnOrder[CurrentTurnIndex]);
     }
     private void OnTurnEnd()
     {
-        GameEvents.TriggerRefEvent(new TriggerRef() { Reference = gameplayRef.onTurnEnd, TargetId = TurnOrder[CurrentTurnIndex].GetInstanceID() });
+        GameEvents.TriggerRefEvent(new TriggerRef(new() { gameplayRef.onTurnEnd }, TurnOrder[CurrentTurnIndex].GetInstanceID()));
         DeckManager.Instance.EndTurn(TurnOrder[CurrentTurnIndex]);
 
         CurrentTurnIndex++;
