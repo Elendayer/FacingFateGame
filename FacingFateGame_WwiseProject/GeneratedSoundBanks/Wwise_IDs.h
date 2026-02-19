66c24ccd20d95f3fc75ad1f171414e267ff0507d
SchieweP <ps.yt.2510@gmail.com>
2026-02-19 18:07:28 +0100

    [edit] audio routing

--- FacingFateGame_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
+++ FacingFateGame_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,8 +13,23 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID PLAY_WS_BACK_TO_TITLE_SCREEN = 4012838884U;
+        static const AkUniqueID END_GAME = 3717848113U;
+        static const AkUniqueID PLAY_BACK_TO_TITLE_SCREEN = 1055782521U;
+        static const AkUniqueID PLAY_UI_ALL_BUTTONS = 1975323918U;
+        static const AkUniqueID START_GAME = 1114964412U;
+        static const AkUniqueID START_OVERWORLD = 3224649440U;
+        static const AkUniqueID START_SCENE_01 = 851434184U;
+        static const AkUniqueID START_TITLESCREEN = 1652174372U;
     } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID RTPC_ATMO_VOL = 1766928022U;
+        static const AkUniqueID RTPC_DIALOGUE_VOL = 3553633719U;
+        static const AkUniqueID RTPC_MASTER_VOL = 460070935U;
+        static const AkUniqueID RTPC_MUSIC_VOL = 3061620274U;
+        static const AkUniqueID RTPC_SFX_VOL = 3502123226U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
@@ -24,8 +39,22 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID BUS_ATMO = 1437783383U;
+        static const AkUniqueID BUS_DIALOGUE = 2216040922U;
+        static const AkUniqueID BUS_MUSIC = 1162281553U;
+        static const AkUniqueID BUS_SFX = 3895923845U;
+        static const AkUniqueID DX_2D_NARRATOR = 857912162U;
+        static const AkUniqueID DX_3D_NPC = 1552314449U;
         static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
+        static const AkUniqueID MX_OVERWORLD = 3132888017U;
+        static const AkUniqueID MX_TRAININGSGROUND = 691797427U;
+        static const AkUniqueID SFX_UI = 3862737079U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID AUX_HALL_BIG = 1039155902U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
